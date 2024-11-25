b52eb560005ab795f66c464ebce0da30fc6ecc5e
Guilherme Borim <borimdev@gmail.com>
2024-11-25 18:21:35 -0300

    test

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,14 @@ import type { JestConfigWithTsJest } from "ts-jest";
 
 const config: JestConfigWithTsJest = {
   collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   preset: "ts-jest/presets/default-esm",
